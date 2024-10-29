8023782afb1e9a94c6e9053b39efaba607cc7b57
Rodrigo Bunhak <rodrigo.bunhak@gmail.com>
2024-10-29 20:08:09 -0300

    Add UpdateProduct use case integration test

---
+++ src/usecase/product/update/update.product.integration.spec.ts
@@ -0,0 +1,35 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import UpdateProductUseCase from "./update.product.usecase";
+import Product from "../../../domain/product/entity/product";
+
+describe("Integration test update product use case", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should update a product", async () => {
+    const productRepository = new ProductRepository();
+    const usecase = new UpdateProductUseCase(productRepository);
+    const product = new Product("123", "Product 1", 25);
+    await productRepository.create(product);
+    const input = { id: product.id, name: "Product 1 updated", price: 15}
+    const output = await usecase.execute(input);
+    expect(output).toEqual(input);
+  })
+})
